4a08a84f2cdf3ebb3edc63ea0026d1455e5773e4
BernardoGiordano <bernardogiordano369@gmail.com>
2026-01-29 22:02:51 +0100

    Initialize empty app components

---
+++ src/app/components/graph-funnel/graph-funnel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graph-funnel',
+  imports: [],
+  templateUrl: './graph-funnel.html',
+  styleUrl: './graph-funnel.css',
+})
+export class GraphFunnel {}
---
+++ src/app/components/graph-projection/graph-projection.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graph-projection',
+  imports: [],
+  templateUrl: './graph-projection.html',
+  styleUrl: './graph-projection.css',
+})
+export class GraphProjection {}
