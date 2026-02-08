7679a65c4c0fc9c9e9bfb75c91842b072b1dc220
BernardoGiordano <bernardogiordano369@gmail.com>
2026-02-08 12:50:19 +0100

    Integrate addizionali data from the start of known data

--- data/generate-comunali.sh
+++ data/generate-comunali.sh
@@ -1,4 +1,13 @@
-python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2010.comunali.ts Add_comunale_irpef2010.csv
+python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2001.comunali.ts Add_comunale_irpef2001.csv
+python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2002.comunali.ts --update ../src/calculator/addizionali/2001.comunali.ts Add_comunale_irpef2002.csv
+python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2003.comunali.ts --update ../src/calculator/addizionali/2002.comunali.ts Add_comunale_irpef2003.csv
+python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2004.comunali.ts --update ../src/calculator/addizionali/2003.comunali.ts Add_comunale_irpef2004.csv
+python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2005.comunali.ts --update ../src/calculator/addizionali/2004.comunali.ts Add_comunale_irpef2005.csv
+python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2006.comunali.ts --update ../src/calculator/addizionali/2005.comunali.ts Add_comunale_irpef2006.csv
+python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2007.comunali.ts --update ../src/calculator/addizionali/2006.comunali.ts Add_comunale_irpef2007.csv
+python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2008.comunali.ts --update ../src/calculator/addizionali/2007.comunali.ts Add_comunale_irpef2008.csv
+python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2009.comunali.ts --update ../src/calculator/addizionali/2008.comunali.ts Add_comunale_irpef2009.csv
+python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2010.comunali.ts --update ../src/calculator/addizionali/2009.comunali.ts Add_comunale_irpef2010.csv
 python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2011.comunali.ts --update ../src/calculator/addizionali/2010.comunali.ts Add_comunale_irpef2011.csv
 python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2012.comunali.ts --update ../src/calculator/addizionali/2011.comunali.ts Add_comunale_irpef2012.csv
 python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2013.comunali.ts --update ../src/calculator/addizionali/2012.comunali.ts Add_comunale_irpef2013.csv
@@ -15,6 +24,15 @@ python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2023.co
 python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2024.comunali.ts --update ../src/calculator/addizionali/2023.comunali.ts Add_comunale_irpef2024.csv
 python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2025.comunali.ts --update ../src/calculator/addizionali/2024.comunali.ts Add_comunale_irpef2025.csv
 python3 convert_addizionali_comunali.py -o ../src/calculator/addizionali/2026.comunali.ts --update ../src/calculator/addizionali/2025.comunali.ts Add_comunale_irpef2026.csv
+rm ../src/calculator/addizionali/2001.comunali.ts
+rm ../src/calculator/addizionali/2002.comunali.ts
+rm ../src/calculator/addizionali/2003.comunali.ts
+rm ../src/calculator/addizionali/2004.comunali.ts
+rm ../src/calculator/addizionali/2005.comunali.ts
+rm ../src/calculator/addizionali/2006.comunali.ts
+rm ../src/calculator/addizionali/2007.comunali.ts
+rm ../src/calculator/addizionali/2008.comunali.ts
+rm ../src/calculator/addizionali/2009.comunali.ts
 rm ../src/calculator/addizionali/2010.comunali.ts
 rm ../src/calculator/addizionali/2011.comunali.ts
 rm ../src/calculator/addizionali/2012.comunali.ts
